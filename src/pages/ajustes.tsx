33ef4c9ff082e734ced3063a29ace82b358c6973
NiltonDev <jnvlfilho@hotmail.com>
2021-10-15 22:36:52 -0300

    Adicionando páginas e itens no Menu lateral para navegação.

---
+++ src/pages/ajustes.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Layout from '../components/template/Layout'
+export default function Ajustes() {
+  return (
+    <Layout titulo="Ajustes" 
+      subtitulo="Página de ajustes!">
+      <h3>Conteudo</h3>
+    </Layout>
+  )
+}
