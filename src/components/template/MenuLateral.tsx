7509b3d2c8f33ecc8c12ca7b3b57c4f2186b4878
NiltonDev <jnvlfilho@hotmail.com>
2021-10-15 22:56:42 -0300

    Adicionando funcionalidade Loggout sem implementar a lógica, apenas estilizando!

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,12 +1,21 @@
-import { IconeAjustes, IconeInicio } from "../icons";
+import { IconeAjustes, IconeInicio, IconeLogout } from "../icons";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral(){
     return (
-        <aside>
-            <ul>
+        <aside className="flex flex-col">
+            <ul className="flex-grow">
                 <MenuItem url="/" texto="Inicio" icone={IconeInicio}/>
                 <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes}/>
+            </ul>
+            <ul>
+                <MenuItem  texto="Sair" icone={IconeLogout}
+                    onClick={() => console.log("Saindo!")}
+                    className={`
+                        text-red-600
+                        hover:bg-red-400 hover:text-white
+                    `}
+                />
             </ul>
         </aside>
     )
